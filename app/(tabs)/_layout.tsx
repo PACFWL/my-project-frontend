f4f08ba37a35a7c81025617a765085587418dcd1
PACFWL <estudanteprincipalgerali@gmail.com>
2025-01-05 20:27:48 -0300

    feat: "continue"

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,10 +3,17 @@ import React from 'react';
 import { Platform } from 'react-native';
 
 import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+
+import HomeIcon from '../../assets/images/game.svg';
+import ExploreIcon from '../../assets/images/sound.svg';
+import ProfileIcon from '../../assets/images/profile.svg';
+import VoiceIcon from '../../assets/images/voice.svg';
+import GameIcon from '../../assets/images/game.svg';
+import MovieIcon from '../../assets/images/movie.svg';
+import CGIcon from '../../assets/images/cg_illustrations.svg';
+import MangaIcon from '../../assets/images/manga.svg';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -17,27 +24,93 @@ export default function TabLayout() {
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
         tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
-            // Use a transparent background on iOS to show the blur effect
             position: 'absolute',
           },
           default: {},
         }),
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <HomeIcon width={28} height={28} fill={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
           title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <ExploreIcon width={28} height={28} fill={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color }) => (
+            <ProfileIcon width={28} height={28} fill={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="voiceScreen"
+        options={{
+          title: 'Voice',
+          tabBarIcon: ({ color }) => (
+            <VoiceIcon width={28} height={28} fill={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="gameScreen"
+        options={{
+          title: 'Game',
+          tabBarIcon: ({ color }) => (
+            <GameIcon width={28} height={28} fill={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="movieScreen"
+        options={{
+          title: 'Movie',
+          tabBarIcon: ({ color }) => (
+            <MovieIcon width={28} height={28} fill={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="cgIllustrationsScreen"
+        options={{
+          title: 'CG+Illustrations',
+          tabBarIcon: ({ color }) => (
+            <CGIcon width={28} height={28} fill={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="mangaScreen"
+        options={{
+          title: 'Manga',
+          tabBarIcon: ({ color }) => (
+            <MangaIcon width={28} height={28} fill={color} />
+          ),
+        }}
+      />
+       <Tabs.Screen
+        name="aiScreen"
+        options={{
+          title: 'AI',
+          tabBarIcon: ({ color }) => (
+            <MangaIcon width={28} height={28} fill={color} />
+          ),
         }}
       />
     </Tabs>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -15,41 +15,6 @@ export default function HomeScreen() {
           style={styles.reactLogo}
         />
       }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
     </ParallaxScrollView>
   );
 }
@@ -58,17 +23,18 @@ const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    gap: 8,
+    gap: 50,
   },
   stepContainer: {
-    gap: 8,
+    gap: 50,
     marginBottom: 8,
   },
   reactLogo: {
-    height: 178,
+    height: 80,
     width: 290,
-    bottom: 0,
+    bottom: 10,
     left: 0,
     position: 'absolute',
+    right: 20,
   },
 });
